94a5f4893e27f6fd12d228bf38916f8afcc8b49d
kehinde <kehinde@learnflo.co>
2025-04-02 09:10:52 +0100

    add blog item page

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
 import { assets, blog_data } from '@/Assets/assets';
+import Link from 'next/link';
 
-const BlogItem = ({title, description, category, image}) => {
+const BlogItem = ({title, description, category, image, id}) => {
     return(
         <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+
+            <Link href={`/blogs/${id}`}>
             <Image src={image} alt='' width={400} height={400} border border-black/>
+            </Link>
+
             <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
 
             <div className='p-5'>
                 <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
                 <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
 
-                <div className='inline-flex items-center py-2 font-semibold text-center'>
+                <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
                     Read more <Image src={assets.arrow} className='ml-2' alt='' width={12}/>
-                </div>
+                </Link>
             </div>
 
             {/* <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'></div> */}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client'
 import Header from "@/Components/Header";
 import BlogList from "@/Components/BlogList";
-
+import Footer from "@/Components/Footer"
 export default function Home() {
   return (
     <>
     <Header/>
     <BlogList/>
+    <Footer/>
     </>
   )
 }
